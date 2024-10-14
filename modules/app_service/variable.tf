a3c6585d90c097f8c1f459b9531edd55cdf6279a
singamlikhitha <likhitha.singam@zionclouds.com>
2024-10-14 15:47:06 +0530

    adds github workflow

--- modules/app_service/variable.tf
+++ modules/app_service/variable.tf
@@ -1,43 +1,43 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
+# variable "resource_group_name" {
+#   description = "The name of the resource group."
+#   type        = string
+# }
 
-variable "location" {
-  description = "The location where the resource group should exist."
-  type        = string
-}
+# variable "location" {
+#   description = "The location where the resource group should exist."
+#   type        = string
+# }
 
-variable "app_service_plan" {
-  description = "The name of the App Service Plan."
-  type        = string
-}
+# variable "app_service_plan" {
+#   description = "The name of the App Service Plan."
+#   type        = string
+# }
 
-variable "app_service_name" {
-  description = "The name of the App Service."
-  type        = string
-}
+# variable "app_service_name" {
+#   description = "The name of the App Service."
+#   type        = string
+# }
 
-variable "sku_tier" {
-  description = "The SKU tier for the App Service Plan."
-  type        = string
-  default     = "Standard"
-}
+# variable "sku_tier" {
+#   description = "The SKU tier for the App Service Plan."
+#   type        = string
+#   default     = "Standard"
+# }
 
-variable "sku_size" {
-  description = "The SKU size for the App Service Plan."
-  type        = string
-  default     = "S1"
-}
+# variable "sku_size" {
+#   description = "The SKU size for the App Service Plan."
+#   type        = string
+#   default     = "S1"
+# }
 
-variable "sku_capacity" {
-  description = "The number of instances for the App Service Plan."
-  type        = number
-  default     = 1
-}
+# variable "sku_capacity" {
+#   description = "The number of instances for the App Service Plan."
+#   type        = number
+#   default     = 1
+# }
 
-variable "app_settings" {
-  description = "A map of application settings for the App Service."
-  type        = map(string)
-  default     = {}
-}
+# variable "app_settings" {
+#   description = "A map of application settings for the App Service."
+#   type        = map(string)
+#   default     = {}
+# }
--- variables.tf
+++ variables.tf
@@ -8,67 +8,67 @@ variable "location" {
   type = string
  
 }
-# variable "tags" {
-#    description = "A map of tags to assign to resources"
-#   type=map(string)
-# }
-# variable "name" {
-#   description = "The name of the storage account"
-#   type        = string
-# }
-
-
-# variable "storageLocation" {
-#   description = "The location/region where the storage account will be created"
-#   type        = string
-# }
-
-# variable "container_name" {
-#   description = "The name of the storage container"
-#   type        = string
-# }
-# variable "vnet_name" {
-#   description = "The name of the Virtual Network"
-#   type        = string
-# }
-
-# variable "subnet_name" {
-#   description = "The name of the Subnet"
-#   type        = string
-# }
-# variable "vnetLocation" {
-#   description = "The location/region where the vnet account will be created"
-#   type        = string
-# }
-variable "app_service_name" {
-  description = "The name of the App Service."
+variable "tags" {
+   description = "A map of tags to assign to resources"
+  type=map(string)
+}
+variable "name" {
+  description = "The name of the storage account"
   type        = string
 }
 
-variable "sku_tier" {
-  description = "The SKU tier for the App Service Plan."
+
+variable "storageLocation" {
+  description = "The location/region where the storage account will be created"
   type        = string
-  
 }
 
-variable "sku_size" {
-  description = "The SKU size for the App Service Plan."
+variable "container_name" {
+  description = "The name of the storage container"
   type        = string
-  
+}
+variable "vnet_name" {
+  description = "The name of the Virtual Network"
+  type        = string
 }
 
-variable "sku_capacity" {
-  description = "The number of instances for the App Service Plan."
-  type        = number
-  
-}
-
-variable "app_settings" {
-  description = "A map of application settings for the App Service."
-  type        = map(string)
-  
-}
-variable "app_service_plan" {
-  description = "the name of the app service plan"
+variable "subnet_name" {
+  description = "The name of the Subnet"
   type        = string
-  }
+}
+variable "vnetLocation" {
+  description = "The location/region where the vnet account will be created"
+  type        = string
+}
+# variable "app_service_name" {
+#   description = "The name of the App Service."
+#   type        = string
+# }
+
+# variable "sku_tier" {
+#   description = "The SKU tier for the App Service Plan."
+#   type        = string
+  
+# }
+
+# variable "sku_size" {
+#   description = "The SKU size for the App Service Plan."
+#   type        = string
+  
+#}
+
+# variable "sku_capacity" {
+#   description = "The number of instances for the App Service Plan."
+#   type        = number
+  
+# }
+
+# variable "app_settings" {
+#   description = "A map of application settings for the App Service."
+#   type        = map(string)
+  
+# }
+# variable "app_service_plan" {
+#   description = "the name of the app service plan"
+#   type        = string
+#   }
