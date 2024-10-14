5a1d37a1881242df53467fbfc61346407c5f3c17
singamlikhitha <likhitha.singam@zionclouds.com>
2024-10-14 17:49:51 +0530

    adds backend file

---
+++ backend-config.hcl
@@ -0,0 +1,4 @@
+resource_group_name = "myresourcegroup7569"
+storage_account_name  = "mystorage7997"
+container_name        = "mycontainer0"
+key                   = "terraform.tfstate"    
--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 #create a resource group
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+  backend "azurerm" {}
+}
 provider "azurerm" {
   features {
     
